9b2a835a20f60ed5974c4073f9bb94d1573b92a2
brendanvanbreda <brendanvanbreda1993@gmail.com>
2018-10-16 14:46:35 +0200

    update src

--- src/arduino-SI01.h
+++ src/arduino-SI01.h
@@ -11,8 +11,8 @@
 	LSM9DS1 - http://www.st.com/content/ccc/resource/technical/document/datasheet/1e/3f/2a/d6/25/eb/48/46/DM00103319.pdf/files/DM00103319.pdf/jcr:content/translations/en.DM00103319.pdf
 */
 
-#ifndef Arduino_SI01_h
-#define Arduino_SI01_h
+#ifndef arduino-SI01_h
+#define arduino-SI01_h
 
 // System Includes
 #include <inttypes.h>
